beb2f6aaf75937736dcb991ac7125732ffdf8273
edwin0258 <edwin0258@gmail.com>
2015-09-07 23:55:58 +0000

    Link articles to users

--- spec/features/deleting_article_spec.rb
+++ spec/features/deleting_article_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 
 RSpec.feature "Deleting an article" do
   before do
-    @article = Article.create(title: "The first article", body: "body of article")
+    john = User.create(email: "john@example.com", password: "password")
+    login_as(john)
+    @article = Article.create(title: "The first article", body: "body of article", user: john)
   end
   
   scenario "A user deletes an article" do
